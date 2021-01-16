59ff8a88fe9351ec2d9d1d5708fe22807e59625d
abbywake <71293588+abbywake@users.noreply.github.com>
2021-01-15 18:04:43 -0700

    Add project files.

---
+++ wwwroot/js/fanMail.js
@@ -0,0 +1,8 @@
+﻿var submit = document.getElementById('submit')
+
+submit.addEventListener('click', function () {
+    alert('From- ' + document.getElementById("from").value + "," +
+        "Subject = " + document.getElementById("email").value + ", " +
+        "Email = " + document.getElementById("subject").value + ", " +
+        "Message = " + document.getElementById("message").value);
+});
