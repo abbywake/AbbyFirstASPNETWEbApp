0ede92d1394d6df38d263842ea056484b560688c
abbywake <71293588+abbywake@users.noreply.github.com>
2021-01-19 13:36:00 -0700

    Commit

--- wwwroot/js/fanMail.js
+++ wwwroot/js/fanMail.js
@@ -1,8 +1,12 @@
 ﻿var submit = document.getElementById('submit')
 
+
 submit.addEventListener('click', function () {
-    alert('From- ' + document.getElementById("from").value + "," +
-        "Subject = " + document.getElementById("email").value + ", " +
-        "Email = " + document.getElementById("subject").value + ", " +
-        "Message = " + document.getElementById("message").value);
+    var assignment = document.getElementById('assignment').value
+    var groupProject = document.getElementById('email').value
+    var quiz = document.getElementById('quiz').value
+    var exam = document.getElementById('exam').value
+    var intex = document.getElementById('intex').value
+
+    alert("Your estimated grade will be " + parseInt(assignment + groupProject + quiz + exam + intex) + "%")
 });
